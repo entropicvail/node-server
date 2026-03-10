945fed85e7979dd0719cb62470e2a221bc0319df
entropicvail <coreycraft@hotmail.com>
2026-03-10 08:13:13 -0700

    Complete first step

--- server.js
+++ server.js
@@ -1,21 +1,7 @@
 import http from 'node:http';
-
 const server = http.createServer((request, response) => {
-  let url = request.url.split('/');
-  if (url[1] === '' ) {
-    if (request.method === 'GET') {
-      response.end("Hello World");
-    }
-  }
-  if (url[1] === 'authors' ) {
-    if (request.method === 'GET') {
-      response.end("CM");
-    }
-  }
-  response.statusCode = 404;
-  response.end();
+      response.end("Hello Galvanize!");
 });
-
 server.listen(3000, "localhost", () => {
-  console.log(`Server running at http://localhost:3000/`);
+  console.log(`Server is running on http://localhost:3000/`);
 });
